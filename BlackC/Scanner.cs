f2cae6cb88231b9142b70c7fd6a9c5c35439bdc0
kohoutech <george@kohoutech.com>
2018-10-12 05:24:17 -0400

    add scanning for octal and hexidecimal integer literal values

--- BlackC/Scanner.cs
+++ BlackC/Scanner.cs
@@ -67,6 +67,150 @@ namespace BlackC
             }
         }
 
+        public Token findKeyword(String id)
+        {
+            Token token = null;
+            switch (id)
+            {
+                case "auto": 
+                    token = new tAuto(); 
+                    break;
+
+                case "break": 
+                    token = new tBreak(); 
+                    break;
+
+                case "case": 
+                    token = new tCase(); 
+                    break;
+
+                case "char": 
+                    token = new tChar(); 
+                    break;
+
+                case "const": 
+                    token = new tConst(); 
+                    break;
+
+                case "continue": 
+                    token = new tContinue(); 
+                    break;
+
+                case "default": 
+                    token = new tDefault(); 
+                    break;
+
+                case "do": 
+                    token = new tDo(); 
+                    break;
+
+                case "double":
+                    token = new tDouble(); 
+                    break;
+
+                case "else": 
+                    token = new tElse(); 
+                    break;
+
+                case "enum": 
+                    token = new tEnum(); 
+                    break;
+
+                case "extern": 
+                    token = new tExtern(); 
+                    break;
+
+                case "float": 
+                    token = new tFloat(); 
+                    break;
+
+                case "for": 
+                    token = new tFor(); 
+                    break;
+
+                case "goto": 
+                    token = new tGoto(); 
+                    break;
+
+                case "if": 
+                    token = new tIf(); 
+                    break;
+
+                case "inline": 
+                    token = new tInline(); 
+                    break;
+
+                case "int": 
+                    token = new tInt(); 
+                    break;
+
+                case "long": 
+                    token = new tLong(); 
+                    break;
+
+                case "register": 
+                    token = new tRegister(); 
+                    break;
+
+                case "restrict": 
+                    token = new tRestrict(); 
+                    break;
+
+                case "return": 
+                    token = new tReturn(); 
+                    break;
+
+                case "short": 
+                    token = new tShort(); 
+                    break;
+
+                case "signed": 
+                    token = new tSigned(); 
+                    break;
+
+                case "sizeof": 
+                    token = new tSizeof(); 
+                    break;
+
+                case "static": 
+                    token = new tStatic(); 
+                    break;
+
+                case "struct": 
+                    token = new tStruct(); 
+                    break;
+
+                case "switch": 
+                    token = new tSwitch(); 
+                    break;
+
+                case "typedef": 
+                    token = new tTypedef(); 
+                    break;
+
+                case "union": 
+                    token = new tUnion(); 
+                    break;
+
+                case "unsigned": 
+                    token = new tUnsigned(); 
+                    break;
+
+                case "void": 
+                    token = new tVoid(); 
+                    break;
+
+                case "volatile": 
+                    token = new tVolatile(); 
+                    break;
+
+                case "while": 
+                    token = new tWhile(); 
+                    break;
+            }
+            return token;
+        }
+
         public Token scanIdentifier(char c)
         {
             String id = "" + c;
@@ -85,19 +229,38 @@ namespace BlackC
                     atend = true;
                 }
             }
-            Token ident = new tIdentifier(id);
-            return ident;
+            Token token = findKeyword(id);
+            if (token == null) 
+            {
+                token = new tIdentifier(id);
+            }
+            return token;
 
         }
 
         public Token scanNumber(char c)
         {
             String num = "" + c;
+            int bass = 10;
+            if (c == '0') 
+            {
+                if ((pos < curline.Length) && (curline[pos] == 'X' || curline[pos] == 'x'))
+                {
+                    bass = 16;
+                    pos++;
+                }
+                else
+                {
+                    bass = 8;
+                }
+            }
             bool atend = !(pos < curline.Length);
             while (!atend)
             {
                 char c1 = curline[pos++];
-                if (c1 >= '0' && c1 <= '9')
+                if (((bass == 10) && (c1 >= '0' && c1 <= '9')) ||
+                    ((bass == 8) && (c1 >= '0' && c1 <= '7')) ||
+                    ((bass == 16) && ((c1 >= '0' && c1 <= '9') || (c1 >= 'A' && c1 <= 'F') || (c1 >= 'a' && c1 <= 'f'))))
                 {
                     num = num + c1;
                     atend = !(pos < curline.Length);
@@ -108,7 +271,7 @@ namespace BlackC
                     atend = true;
                 }
             }
-            int value = Int32.Parse(num);
+            int value = Convert.ToInt32(num, bass);
             Token intconst = new tIntegerConstant(value);
             return intconst;
         }
@@ -257,6 +420,7 @@ namespace BlackC
                         if ((pos < curline.Length - 1) && (curline[pos] == '.') && (curline[pos + 1] == '.'))
                         {
                             token = new tEllipsis();
+                            pos += 2;
                         }
                         else
                         {
@@ -268,10 +432,12 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '+'))
                         {
                             token = new tPlusPlus();
+                            pos++;
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tPlusEqual();
+                            pos++;
                         }
                         else
                         {
@@ -283,14 +449,17 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '-'))
                         {
                             token = new tMinusMinus();
+                            pos++;
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tMinusEqual();
+                            pos++;
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '>'))
                         {
                             token = new tArrow();
+                            pos++;
                         }
                         else
                         {
@@ -302,14 +471,16 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '&'))
                         {
                             token = new tDoubleAmp();
+                            pos++;
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tAmpEqual();
+                            pos++;
                         }
                         else
                         {
-                            token = new tAmpersand();
+                            token = new tAmpersand();                            
                         }
                         break;
 
@@ -317,10 +488,12 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '&'))
                         {
                             token = new tDoubleAmp();
+                            pos++;
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tAmpEqual();
+                            pos++;
                         }
                         else
                         {
@@ -336,6 +509,7 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tNotEqual();
+                            pos++;
                         }
                         else
                         {
@@ -347,6 +521,7 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tSlashEqual();
+                            pos++;
                         }
                         else
                         {
@@ -358,6 +533,7 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tPercentEqual();
+                            pos++;
                         }
                         else
                         {
@@ -371,15 +547,18 @@ namespace BlackC
                             if ((pos < curline.Length - 1) && (curline[pos + 1] == '='))
                             {
                                 token = new tLShiftEqual();
+                                pos += 2;
                             }
                             else
                             {
                                 token = new tLeftShift();
+                                pos++;
                             }
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tLessEqual();
+                            pos++;
                         }
                         else
                         {
@@ -393,15 +572,18 @@ namespace BlackC
                             if ((pos < curline.Length - 1) && (curline[pos + 1] == '='))
                             {
                                 token = new tRShiftEqual();
+                                pos += 2;
                             }
                             else
                             {
                                 token = new tRightShift();
+                                pos++;
                             }
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tGtrEqual();
+                            pos++;
                         }
                         else
                         {
@@ -413,6 +595,7 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tEqualEqual();
+                            pos++;
                         }
                         else
                         {
@@ -424,6 +607,7 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tCaretEqual();
+                            pos++;
                         }
                         else
                         {
@@ -435,10 +619,12 @@ namespace BlackC
                         if ((pos < curline.Length) && (curline[pos] == '|'))
                         {
                             token = new tDoubleBar();
+                            pos++;
                         }
                         else if ((pos < curline.Length) && (curline[pos] == '='))
                         {
                             token = new tBarEqual();
+                            pos++;
                         }
                         else
                         {
